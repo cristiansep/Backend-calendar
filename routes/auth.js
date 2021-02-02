b457548728fa51ba6371f7d5dc3cf6ff2f131041
cristiansep <cristiansepulvedamendez@gmail.com>
2021-02-02 17:39:14 -0300

    new validations added

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const {validarJWT} = require('../middlewares/validar-jwt');
 router.post('/new',[
     
     check('name', 'El nombre es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').isEmail(),
+    check('email', 'Debe ingresar un email valido').isEmail(),
     check('password', 'El password debe ser de 6 caracteres').isLength({min: 6}),
     validarCampos
 
@@ -17,7 +17,9 @@ router.post('/new',[
 
 router.post('/',[
 
-    check('email', 'El email es obligatorio').isEmail(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('email', 'Debe ingresar un email valido ').isEmail(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser de 6 caracteres').isLength({min: 6}),
     validarCampos
 
@@ -28,3 +30,19 @@ router.get('/renew',validarJWT, revalidarToken);
 
 
 module.exports = router;
+
+
+// this.router.post('/new',[ 
+//     check('name', 'El nombre es obligatorio').not().isEmpty(),
+//     check('email', 'El email es obligatorio').isEmail()
+//     .custom(async value => {
+//         const emailCheck = await getUserByEmail(value);
+//         if (!!emailCheck) 
+//           return Promise.reject();                
+//     }).withMessage('Email ya se encuentra registrado'),
+//     check('password')
+//         .isLength({ min: 5 }).withMessage('El password debe ser de 5 caracteres')
+//         .matches(/\d/).withMessage('El password debe contener al menos un número')
+// ], 
+// validator,
+// authController.register);
